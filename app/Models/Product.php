6893277a5ab32dc781b8d2824d71910e7cf6a416
Ashisoma <okalionesmus@gmail.com>
2022-05-16 21:13:21 +0300

    register

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable, HasApiTokens;
+    
     protected $fillable = [
         'name','slug','description','price'
     ]; 
--- routes/api.php
+++ routes/api.php
@@ -15,15 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Protected ROutes
+Route::group(['middleware'=>['auth:sanctum']], function () {
+
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    // Route::post('products', [ProductController::class, 'store']);
+
 });
 
-Route::resource('products', ProductController::class);
+// Public routes
+Route::get('/products/{id}', [ProductController::class, 'show'],);
 
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-// Route::get('/products', [ProductController::class, 'index']);
-
-// Route::post('products', [ProductController::class, 'store']);
- 
+Route::get('/products', [ProductController::class, 'index']);
